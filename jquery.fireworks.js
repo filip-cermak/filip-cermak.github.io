c88ed725673b89385c0a03b37d27a4aa09449cc3
filip-cermak <47925995+filip-cermak@users.noreply.github.com>
2019-12-18 20:52:52 +0100

    change launch freq

--- jquery.fireworks.js
+++ jquery.fireworks.js
@@ -267,7 +267,7 @@
 
     // Append the canvas and start the loops
     $(fireworksField).append(canvas);
-    setInterval(launch, 800);
+    setInterval(launch, 300);
     setInterval(loop, 1000 / 50);
 
     return fireworksField;
